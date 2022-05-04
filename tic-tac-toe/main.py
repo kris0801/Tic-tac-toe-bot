699871733855549d255f025af8e2e7ff737a1933
kris0801 <kristopherpaz@ufm.edu>
2022-05-04 17:20:26 -0600

    kk

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -7,5 +7,8 @@ def main():
     player_id = utils.register(name)
     print (player_id)
 
+    my_turn = utils.is_my_turn(player_id)
+    print (my_turn)
+
 if __name__ == '__main__':
     main()
--- tic-tac-toe/utils.py
+++ tic-tac-toe/utils.py
@@ -1,5 +1,10 @@
 '''Utils module'''
-
+#registro
 def register (name: str) -> str:
     player_id = 'X'
     return player_id
+
+
+def is_my_turn(player_id: str)-> bool: 
+    my_turn = True
+    return my_turn
